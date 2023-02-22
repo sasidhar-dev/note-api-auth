42f5dca2b65851d76b53470f456b5ed1d3c6f7a8
Sasidharan <sasidharansrinivasan@gmail.com>
2023-02-22 15:43:31 +0530

    v1

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "notesapiauth": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "notesauthapib82f3d7f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "notesauthapi55372ed0": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import  { API } from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+// function App({ signOut }) {
+//   return (
+//     <View className="App">
+      
+//       <Button onClick={signOut}>Sign Out</Button>
+//     </View>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+const myAPI = 'notesapiauth';
+const path = '/notes';
+
+function App({ signOut }) {
+  const [input, setInput] = useState('');
+  const [notes, setNotes] = useState([]);
+
+  function addNote() {
+    const newNote = { 'id': Date.now().toString(), 'content': input };
+    API.post(myAPI, path, { body: newNote })
+      .then(response => {
+        console.log(response);
+        setNotes([...notes, newNote]); // Add the new note to the current notes array
+        setInput(''); // Clear the input field
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    API.get(myAPI, path)
+      .then(response => {
+        console.log(response);
+        setNotes(response); // Update the notes array with the data from the API
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notes App</h1>
+      <div>
+        <input placeholder="Note content" type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={() => addNote()}>Add Note</button>
+      </div>
+      <br />
+      <Button onClick={signOut}>Sign Out</Button>
+      {
+        notes.map((note, index) => {
+          return (
+            <div key={note.id}>
+              <span>{note.content}</span>
+              
+            </div>
+            
+          );
+        })
+      }
     </div>
   );
-}
+};
+export default withAuthenticator(App);
 
-export default App;
