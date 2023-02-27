2431f51480798ab423fe58d7f257961e7864248d
Sasidharan <sasidharansrinivasan@gmail.com>
2023-02-27 11:33:38 +0530

    V3 with apiauth

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import './App.css';
-import  { API } from 'aws-amplify';
+import  { API, Auth } from 'aws-amplify';
 import React, { useEffect, useState } from 'react';
 
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
   Button,
-  Heading,
-  Image,
-  View,
-  Card,
 } from "@aws-amplify/ui-react";
-
-// function App({ signOut }) {
-//   return (
-//     <View className="App">
-      
-//       <Button onClick={signOut}>Sign Out</Button>
-//     </View>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
 
 const myAPI = 'notesapiauth';
 const path = '/notes';
@@ -40,7 +17,14 @@ function App({ signOut }) {
 
   function addNote() {
     const newNote = { 'id': Date.now().toString(), 'content': input };
-    API.post(myAPI, path, { body: newNote })
+    Auth.currentSession()
+      .then(session => {
+        const token = session.getIdToken().getJwtToken();
+        return API.post(myAPI, path, {
+          headers: { Authorization: token },
+          body: newNote
+        });
+      })
       .then(response => {
         console.log(response);
         setNotes([...notes, newNote]); // Add the new note to the current notes array
@@ -50,16 +34,21 @@ function App({ signOut }) {
         console.log(error);
       });
   }
+  
 
   useEffect(() => {
-    API.get(myAPI, path)
-      .then(response => {
-        console.log(response);
-        setNotes(response); // Update the notes array with the data from the API
-      })
-      .catch(error => {
+    async function fetchNotes() {
+      try {
+        const session = await Auth.currentSession();
+        const token = session.getIdToken().getJwtToken();
+        const notesResponse = await API.get(myAPI, path, { headers: { Authorization: token } });
+        console.log(notesResponse); 
+        setNotes(notesResponse); // Update the notes array with the data from the API
+      } catch (error) {
         console.log(error);
-      });
+      }
+    }
+    fetchNotes();
   }, []);
 
   return (
@@ -86,4 +75,3 @@ function App({ signOut }) {
   );
 };
 export default withAuthenticator(App);
-
